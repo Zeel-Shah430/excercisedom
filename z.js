fd4704a19bd764e2f6a814db61b059bce5c85d41
Zeel-Shah430 <zeelshah430@gmail.com>
2024-12-29 13:12:52 +0530

    Add files via upload

---
+++ z.js
@@ -0,0 +1,11 @@
+let c=document.getElementById('wth');
+let d=document.getElementById('wth2');
+
+function validation()
+{
+
+if(c.value !== d.value)
+				{
+					alert("both passwords are not same");
+				}
+}
